326a21ef6320c0f6d176a7b23ecfff8225fd8c43
tracer-ics2017 <tracer@njuics.org>
2020-02-28 14:57:14 +0800

    >  compile 1711326 hrl Linux hrl 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1 (2020-01-26) x86_64 GNU/Linux  14:57:14 up  5:11,  2 users,  load average: 0.77, 0.73, 0.44 dccd7c87ce9de7c585d3e8b5e2bcdfceff4143ef

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -268,6 +268,7 @@ int eval(uint32_t p, uint32_t q) {
     // 一元运算符
     else {
       int expr = eval(p + 1, q);
+      if (expr == BAD_EXPR) return BAD_EXPR;
       switch (tokens[dominant_op_index].type) {
       case TK_NEG:
         return -expr;
