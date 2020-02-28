dc8eead7d151382b2f53e79c8e0ece23ba2c35d9
tracer-ics2017 <tracer@njuics.org>
2020-02-28 13:20:19 +0800

    >  compile 1711326 hrl Linux hrl 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1 (2020-01-26) x86_64 GNU/Linux  13:20:19 up  3:34,  2 users,  load average: 1.01, 0.83, 0.71 f924188967d04c7aee64e0b6375d3a69d12daf7b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -96,18 +96,19 @@ __cr cmd_x (char *args) {
     Log("\033[0;31m""Bad Expression!\n""\033[0m");
     return 0;
   }
-  printf("Memory:\n");
+  printf("Memory:");
   int i;
   for (i=0; i < n; i++)
   {
     int value = vaddr_read(addr+i, 1);
     if (i%4 == 0) {
-      printf("\n0x08%x:  0x%02x", addr+i, value);
+      printf("\n0x%08x:  0x%02x", addr+i, value);
     }
     else {
       printf("  0x%02x", value);
     }
   }
+  printf("\n");
   return 0;
 }
 
