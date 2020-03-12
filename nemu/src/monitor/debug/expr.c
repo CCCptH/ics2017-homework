e08d37a3fea4d5bbcfce073261cdf9a8ecbc22e0
tracer-ics2017 <tracer@njuics.org>
2020-03-12 13:43:39 +0800

    >  compile 1711326 hrl Linux hrl 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1 (2020-01-26) x86_64 GNU/Linux  13:43:39 up  2:11,  3 users,  load average: 0.87, 0.44, 0.48 995aed2cd1dab575562ebb0d55e7ae7712252a4

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -352,13 +352,10 @@ uint32_t get_dominant_op_index(uint32_t p, uint32_t q) {
   uint32_t dominant = 0;
   uint32_t dominant_index = p;
   for (i = p; i <= q; i++) {
-    printf("token type is %c\n", tokens[i].type);
-    printf("fuck it %d", tokens[i].type == '(');
-    printf("parentheses_flag is %d\n", parentheses_flag);
+    if (tokens[i].type == '(') ++parentheses_flag;
+    else if (tokens[i].type == ')') --parentheses_flag;
     if (!is_operator(&tokens[i])) continue;
     else if (parentheses_flag != 0) continue;
-    else if (tokens[i].type == '(') parentheses_flag = parentheses_flag + 1;
-    else if (tokens[i].type == ')') parentheses_flag = parentheses_flag - 1;
     else {
       if (get_priority(tokens[i].type) >= get_priority(dominant)) {
         dominant = tokens[i].type;
