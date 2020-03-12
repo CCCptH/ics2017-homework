62836796a45fb894009999639467c2eadc821780
tracer-ics2017 <tracer@njuics.org>
2020-03-12 12:47:47 +0800

    >  compile 1711326 hrl Linux hrl 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1 (2020-01-26) x86_64 GNU/Linux  12:47:47 up  1:15,  3 users,  load average: 0.47, 0.43, 0.42 f5f8ff6f39afa3f114cbc3fabf8c8d6d338e52b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -310,7 +310,7 @@ int8_t check_parentheses(uint32_t p, uint32_t q) {
       flag = 0;  // 整个表达式没被括号包裹
       break;
     }
-    if (stack < 0) {
+    else if (stack < 0) {
       flag = -1;           // 右括号多了， 表达式不对
       printf("unmatched parenthese )\n");
       break;
