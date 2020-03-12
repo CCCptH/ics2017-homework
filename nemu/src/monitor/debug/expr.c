c7462bb23453ee0b2a001e8db7ebfb1dd7a51a66
tracer-ics2017 <tracer@njuics.org>
2020-03-12 13:24:59 +0800

    >  compile 1711326 hrl Linux hrl 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1 (2020-01-26) x86_64 GNU/Linux  13:24:59 up  1:53,  3 users,  load average: 0.57, 0.62, 0.57 8fbe404e53800639f3a1438bf8da126d57d74384

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -352,7 +352,7 @@ uint32_t get_dominant_op_index(uint32_t p, uint32_t q) {
   uint32_t dominant = 0;
   uint32_t dominant_index = p;
   for (i = p; i <= q; i++) {
-    printf("token type is %d", tokens[i].type);
+    printf("token type is %d\n", tokens[i].type);
     if (!is_operator(&tokens[i])) continue;
     else if (parentheses_flag != 0) continue;
     else {
