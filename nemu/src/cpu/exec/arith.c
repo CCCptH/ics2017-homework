e5a2e996f7047fa146351a718ce1a57bf5b86cce
1711326-郝润来 <993855014@qq.com>
2020-03-15 13:13:46 +0800

    make_EHelper(dec)

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -173,6 +173,26 @@ make_EHelper(dec) {
    *    ZF, SF, OF, CF
    */
 
+  // evaluate
+  rtl_subi(&t2, &id_dest->val, 1);
+
+  // write
+  operand_write(id_dest, &t2);
+
+  // update ZF, SF
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  // CF
+  rtl_sltu(&t0, &id_dest->val, &t2);
+  rtl_set_CF(&t0);
+
+  // OF
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
+
   print_asm_template1(dec);
 }
 
