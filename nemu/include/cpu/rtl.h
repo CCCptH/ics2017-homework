af03d07128d4940f065137f014a8d7706939cdb4
1711326-郝润来 <993855014@qq.com>
2020-03-17 16:57:01 +0800

    rtl_push(...) finished

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -159,6 +159,8 @@ static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   // TODO();
+  cpu.esp -= 4;
+  vaddr_write(cpu.esp, 4, *src1);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
