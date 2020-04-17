794a5bdfc4a0a8dd4d44491ecac1180c3eb354a7
tracer-ics2017 <tracer@njuics.org>
2020-04-17 16:55:46 +0800

    >  compile 1711326 hrl Linux hrl 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1 (2020-01-26) x86_64 GNU/Linux  16:55:46 up 1 day,  5:47,  2 users,  load average: 1.14, 1.05, 0.68 5cfda89e7b58cc4f6adcfbd17cb928f184183b8d

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -15,13 +15,27 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:
+      *dest = cpu.eflags.OF;
+      break;
     case CC_B:
+      *dest = cpu.eflags.CF;
+      break;
     case CC_E:
+      *dest = cpu.eflags.ZF;
+      break;
     case CC_BE:
+      *dest = cpu.eflags.ZF || cpu.eflags.CF;
+      break;
     case CC_S:
+      *dest = cpu.eflags.SF;
+      break;
     case CC_L:
+      *dest = cpu.eflags.SF != cpu.eflags.OF;
+      break;
     case CC_LE:
-      TODO();
+      // TODO();
+			*dest = ((cpu.eflags.ZF) || (cpu.eflags.SF != cpu.eflags.OF));
+			break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
