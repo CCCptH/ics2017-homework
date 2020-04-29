14f123cd75edeb5ccb7edccf1ce36027aec4c607
tracer-ics2017 <tracer@njuics.org>
2020-04-29 15:14:39 +0800

    >  compile 1711326 hrl Linux hrl 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1 (2020-01-26) x86_64 GNU/Linux  15:14:39 up 16:17,  3 users,  load average: 1.21, 0.74, 0.57 cc6f3b25630e2a84b4e861409f2e08bb416dc002

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -56,9 +56,9 @@ void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
   // TODO();
-  t0 = pio_read(id_dest->val, id_dest->width);
-  //Log("%x\n", id_src->width);
-  operand_write(id_dest, &t0);
+  // t0 = pio_read(id_src->val, id_src->width);
+  // operand_write(id_dest, &t0);
+  reg_l(R_EAX) = pio_read(reg_w(R_EDX), id_dest->width);
   print_asm_template2(in);
 
 #ifdef DIFF_TEST
