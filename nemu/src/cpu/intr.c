d3e2f5a28137b11760e16c041160596ba4c64782
tracer-ics2017 <tracer@njuics.org>
2020-05-10 18:00:42 +0800

    >  compile 1711326 hrl Linux hrl 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1+deb10u1 (2020-04-27) x86_64 GNU/Linux  18:00:42 up 50 min,  2 users,  load average: 0.63, 0.89, 0.96 5e70cd2798fcbe37a361a5ce8d68ef848ddc9484

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,7 +5,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-  TODO();
+  // TODO();
   rtl_push(&cpu.flags);
   cpu.eflags.IF = 0;
   cpu.eflags.TF = 0;
