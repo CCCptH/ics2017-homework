daa5a4c2f36383d4ea1d62b2d5694616b51b140a
tracer-ics2017 <tracer@njuics.org>
2020-05-10 17:52:54 +0800

    >  run 1711326 hrl Linux hrl 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1+deb10u1 (2020-04-27) x86_64 GNU/Linux  17:52:54 up 42 min,  2 users,  load average: 1.15, 1.19, 1.03 6f41c368ef2f9812585f132f9d1d270d2bb55811

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,13 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+void ramdisk_read(void *, off_t, size_t);
+void ramdisk_write(const void*, off_t, size_t);
+size_t get_ramdisk_size();
+extern uint8_t ramdisk_start;
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  // TODO();
+  ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
