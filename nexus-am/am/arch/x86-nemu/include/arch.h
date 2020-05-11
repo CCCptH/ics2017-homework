c617719a48b4f3a3e7fe9e899664a15dec6b5a59
tracer-ics2017 <tracer@njuics.org>
2020-05-11 18:39:18 +0800

    >  run 1711326 hrl Linux hrl 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1+deb10u1 (2020-04-27) x86_64 GNU/Linux  18:39:18 up  6:31,  2 users,  load average: 0.89, 0.77, 0.63 d998279837f406506bfdc39d7eae748ca059

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -7,12 +7,6 @@
 #define PGSIZE    4096    // Bytes mapped by a page
 
 struct _RegSet {
-  uintptr_t 
-            error_code,
-            eip, 
-            CS,
-            eflags;
-  int       irq;    
   uintptr_t         // pushal
             edi,    // low addr
             esi,    //
@@ -22,12 +16,18 @@ struct _RegSet {
             edx,    //
             ecx,    //
             eax;    // high addr
+  int       irq;    
+  uintptr_t 
+            error_code,
+            eip, 
+            CS,
+            eflags;
 };
 
-#define SYSCALL_ARG1(r) 0
-#define SYSCALL_ARG2(r) 0
-#define SYSCALL_ARG3(r) 0
-#define SYSCALL_ARG4(r) 0
+#define SYSCALL_ARG1(r) r->eax
+#define SYSCALL_ARG2(r) r->ebx
+#define SYSCALL_ARG3(r) r->ecx
+#define SYSCALL_ARG4(r) r->edx
 
 #ifdef __cplusplus
 extern "C" {
