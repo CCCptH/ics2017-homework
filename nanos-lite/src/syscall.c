f85aec835cf9c2056633181b225677b392b07681
tracer-ics2017 <tracer@njuics.org>
2020-05-11 19:03:27 +0800

    >  run 1711326 hrl Linux hrl 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1+deb10u1 (2020-04-27) x86_64 GNU/Linux  19:03:27 up  6:55,  2 users,  load average: 0.94, 0.45, 0.48 497d90554eacf23c93fb435a1585a683e8871e4f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,6 +9,8 @@ _RegSet* do_syscall(_RegSet *r) {
   a[3] = SYSCALL_ARG1(r);
 
   switch (a[0]) {
+    case SYS_none:
+      SYSCALL_ARG1(r) = 1;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
