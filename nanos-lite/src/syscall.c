1f68e00515d7297137cd22d38c913bbb8acc74e3
tracer-ics2017 <tracer@njuics.org>
2020-05-31 23:17:11 +0800

    >  run 1711326 hrl Linux hrl 5.6.0-2-amd64 #1 SMP Debian 5.6.14-1 (2020-05-23) x86_64 GNU/Linux  23:17:11 up  2:07,  3 users,  load average: 1.87, 1.11, 0.85 b765f207ff8acbb5b8ed0c4819c524446f40ce95

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,8 @@
 #include "common.h"
 #include "syscall.h"
 
+
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -15,6 +17,27 @@ _RegSet* do_syscall(_RegSet *r) {
     case SYS_exit:
       _halt(SYSCALL_ARG2(r));
       break;
+    case SYS_write:
+    {
+      int rs;
+      int fd = a[1];
+      void* buf = (void*)a[2];
+      size_t len = a[3];
+      if (fd == 1 || fd == 2) {
+        char c;
+        for (int i=0;i<len;i++) {
+          memcpy(&c, buf+i, 1);
+          _putc(c);
+        }
+        rs = len;
+      }
+      else {
+        panic("sys_write error, invalid fd\n");
+        rs = -1;
+      }
+      SYSCALL_ARG1(r) = rs;
+    }
+    break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
