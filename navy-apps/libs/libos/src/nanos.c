3d9480e021b435ae4ab5cbfedfd22fb65a9b3eaf
tracer-ics2017 <tracer@njuics.org>
2020-05-31 23:41:50 +0800

    >  run 1711326 hrl Linux hrl 5.6.0-2-amd64 #1 SMP Debian 5.6.14-1 (2020-05-23) x86_64 GNU/Linux  23:41:50 up  2:32,  3 users,  load average: 0.80, 0.91, 0.93 efa17b58cc711831dd525eec4c6ff44e8a6cbae

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -31,8 +31,8 @@ int _write(int fd, void *buf, size_t count){
 }
 
 void *_sbrk(intptr_t increment){
-  extern end;
-  static uintptr_t probrk = (uintptr_t)&end;
+  extern _end;
+  static uintptr_t probrk = (uintptr_t)&_end;
   uintptr_t probrk_new = probrk+increment;
   int r = _syscall_(SYS_brk, probrk_new, 0, 0);
   if (r==0) {
