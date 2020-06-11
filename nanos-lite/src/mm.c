9c0622b9c76fc55bda472adcfc830bd59a595a50
tracer-ics2017 <tracer@njuics.org>
2020-06-11 20:59:02 +0800

    >  run 1711326 hrl Linux hrl 5.6.0-2-amd64 #1 SMP Debian 5.6.14-1 (2020-05-23) x86_64 GNU/Linux  20:59:02 up 2 days, 10:55,  2 users,  load average: 0.44, 0.52, 0.74 c50abe7436f754bafcd0612d1eef11ec3bc51e72

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -17,28 +17,24 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uint32_t new_brk) {
-	//Log("in the mm_brk!");
-	if (current->cur_brk == 0) {
-		current->cur_brk = current->max_brk = new_brk;
-	}
-	else {
-		if (new_brk > current->max_brk) {
-			// TODO: map memory region [current->max_brk, new_brk)
-			// into address space current->as
-			void *page;
-			int len = new_brk - current->max_brk;
-			uintptr_t va = PGROUNDUP(current->max_brk);
-			while (len > 0) {
-				page = new_page();
-				_map(&current->as, (void *)va, page);
-				va += PGSIZE;
-				len -= PGSIZE;
-			}
-			current->max_brk = new_brk;
-		}
-		current->cur_brk = new_brk;
-	}
-	return 0;
+  // return 0;
+  if (current->cur_brk == 0) {
+    current->cur_brk = current->max_brk = new_brk;
+  }
+  else {
+    if (new_brk > current->max_brk) {
+      uintptr_t begin = PGROUNDUP(current->max_brk);
+      uintptr_t end = PGROUNDDOWN(new_brk);
+      if ((new_brk & 0xfff) == 0) end-=PGSIZE;
+      for (uintptr_t va = begin; va <= end; va+=PGSIZE) {
+        void* pg = new_page();
+        _map(&current->as, (void*)va, pg);
+      }
+      current->max_brk = new_brk;
+    }
+    current->cur_brk = new_brk;
+  }
+  return 0;
 }
 
 void init_mm() {
