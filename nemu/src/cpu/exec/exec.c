236bd62b85ed69f09e2d5293d8b14449aa893504
tracer-ics2017 <tracer@njuics.org>
2020-06-11 19:15:51 +0800

    >  compile 1711326 hrl Linux hrl 5.6.0-2-amd64 #1 SMP Debian 5.6.14-1 (2020-05-23) x86_64 GNU/Linux  19:15:51 up 2 days,  9:12,  2 users,  load average: 0.73, 1.10, 1.01 58f5b0749c9b9987ac978565861385045ca64e4c

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -133,7 +133,7 @@ opcode_entry opcode_table [512] = {
   /* 0xd4 */	EMPTY, EMPTY, EX(nemu_trap), EMPTY,
   /* 0xd8 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xdc */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xe0 */	EMPTY, EMPTY, EMPTY, IDEX(J, jcc),
+  /* 0xe0 */	EMPTY, EMPTY, EMPTY, IDEXW(J, jcc, 1),
   /* 0xe4 */	IDEXW(in_I2a, in, 1), IDEX(in_I2a, in), IDEXW(out_a2I, out,1), IDEX(out_a2I, out),
   /* 0xe8 */	IDEX(J, call), IDEX(J, jmp), EMPTY, IDEXW(J, jmp, 1),
   /* 0xec */	IDEXW(in_dx2a, in, 1), IDEX(in_dx2a, in), IDEXW(out_a2dx, out, 1), IDEX(out_a2dx, out),
