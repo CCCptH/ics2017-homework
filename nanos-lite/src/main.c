9d57907de6122f7feffc0cc09bf4129565345f43
tracer-ics2017 <tracer@njuics.org>
2020-06-11 19:50:13 +0800

    >  run 1711326 hrl Linux hrl 5.6.0-2-amd64 #1 SMP Debian 5.6.14-1 (2020-05-23) x86_64 GNU/Linux  19:50:13 up 2 days,  9:46,  2 users,  load average: 0.43, 0.95, 1.69 59dee75343b7c3f5aa1cdb9d68f75e2a7a91012c

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -34,7 +34,7 @@ int main() {
   uint32_t entry = loader(NULL, "/bin/hello");
   ((void (*)(void))entry)();
 #else
-  load_prog("/bin/bmptest");
+  load_prog("/bin/lua");
 #endif
 
   panic("Should not reach here");
