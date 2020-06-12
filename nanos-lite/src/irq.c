a14c2ab74dca9b3ad7bbeb8dce062e8748243eb2
tracer-ics2017 <tracer@njuics.org>
2020-06-12 15:52:23 +0800

    >  run 1711326 hrl Linux hrl 5.6.0-2-amd64 #1 SMP Debian 5.6.14-1 (2020-05-23) x86_64 GNU/Linux  15:52:23 up 3 days,  5:48,  3 users,  load average: 0.45, 0.63, 0.75 65124ca8bf677a4a9ca5aada51783e673dd69d6f

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,14 +6,16 @@ extern _RegSet* schedule(_RegSet *prev);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:
-      return do_syscall(r);
-      //return schedule(r);
+      //return 
+      do_syscall(r);
+      return schedule(r);
     case _EVENT_TRAP:
       Log("Event_trap received!");
       return schedule(r);
     case _EVENT_IRQ_TIME:
       Log("time!");
-      return schedule(r);
+      //return schedule(r);
+      break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
