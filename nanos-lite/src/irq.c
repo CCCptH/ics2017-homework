128175cd70b1191f1c6fdbc522fc87203f3ad81f
tracer-ics2017 <tracer@njuics.org>
2020-06-12 15:45:46 +0800

    >  run 1711326 hrl Linux hrl 5.6.0-2-amd64 #1 SMP Debian 5.6.14-1 (2020-05-23) x86_64 GNU/Linux  15:45:46 up 3 days,  5:42,  3 users,  load average: 0.21, 0.75, 0.83 bd0cd0d193085e292b216053ef7baae7426a1f06

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -8,11 +8,9 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
     case _EVENT_SYSCALL:
       return do_syscall(r);
       //return schedule(r);
-      // break;
     case _EVENT_TRAP:
       Log("Event_trap received!");
       return schedule(r);
-      // break;
     case _EVENT_IRQ_TIME:
       Log("time!");
       return schedule(r);
