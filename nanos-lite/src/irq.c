eeb1eb3b6ad8ef2befe0b6a5f6f9051297fcabf6
tracer-ics2017 <tracer@njuics.org>
2020-06-12 15:41:39 +0800

    >  run 1711326 hrl Linux hrl 5.6.0-2-amd64 #1 SMP Debian 5.6.14-1 (2020-05-23) x86_64 GNU/Linux  15:41:39 up 3 days,  5:37,  3 users,  load average: 1.19, 1.08, 0.90 e0f32670698da9e8a76f6b907a500069b62b4b99

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -14,6 +14,7 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
       return schedule(r);
       // break;
     case _EVENT_IRQ_TIME:
+      Log("time!");
       return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
