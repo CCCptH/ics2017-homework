253302320b794480ad253836e35f32f6393595e8
tracer-ics2017 <tracer@njuics.org>
2020-06-12 15:46:40 +0800

    >  run 1711326 hrl Linux hrl 5.6.0-2-amd64 #1 SMP Debian 5.6.14-1 (2020-05-23) x86_64 GNU/Linux  15:46:40 up 3 days,  5:42,  3 users,  load average: 0.75, 0.81, 0.84 f45505423c81478b1dab8d36471c28749fb6fca2

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,8 +28,8 @@ void load_prog(const char *filename) {
 
 _RegSet* schedule(_RegSet *prev) {
   current->tf = prev;
-  // current = &pcb[0];
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  current = &pcb[0];
+  //current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   /*
   static int counter = 0;
   if (current == &pcb[0] && counter < 300) {
